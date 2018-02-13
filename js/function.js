9b8508c4c113abd8176622b0d5b99b4bedf1c09a
Karina <zhigimont.k@gmail.com>
2018-02-13 23:20:48 +0300

    Add formula validation

--- js/function.js
+++ js/function.js
@@ -4,6 +4,8 @@
 var formula = "";
 var subformulaAnswer = 0;
 var isNeutralAnswer = false;
+var table = [];
+var subformulaCount = 0;
 
 //основной алгоритм
 function run() {
@@ -15,8 +17,8 @@ function run() {
     }
     subformulaAnswer = document.getElementById("subFormulaNumber").value;
     isNeutralAnswer = document.getElementById("neutral").checked;
-    alert(getNumberOfSubformulas());
-
+    subformulaCount = getNumberOfSubformulas();
+    checkAnswer();
 }
 
 //получение числа подформул
@@ -25,7 +27,7 @@ function getNumberOfSubformulas() {
     var letters = [];
     for (var i = 0; i < formula.length; i++) {
         var letter = formula.charAt(i);
-        var regexp = new RegExp("[A-Z0-1]");
+        var regexp = new RegExp("[A-Za-z0-1]");
         if (letter == "(") {
             subformulas++;
         }
@@ -63,18 +65,53 @@ function isFormula() {
     return true;
 }
 
+function fillTruthTable(){
+    for (var i = 0; i < subformulaCount; i++){
+        table[i] = [];
+    }
+}
+
 //сброс
 function clear() {
     document.getElementById("formula").innerHTML = "";
-    document.getElementById("outputField").innerHTML = "";
+    document.getElementById("output-field").innerHTML = "";
     formula = "";
 }
 
 //проверка ответа пользователя
 function checkAnswer() {
-
+    if (subformulaCount == subformulaAnswer){
+        //add class "correct"
+        document.getElementById("output-field").innerHTML = "<p>Subformula number is correct!</p>";
+    } else {
+        //add class "not-correct"
+        document.getElementById("output-field").innerHTML = "<p>You got subformula number wrong, the correct answer is "
+            +subformulaCount+".</p>";
+    }
 }
 
 function showResult() {
 
+}
+
+//валидация введённой формулы
+function isFormula(){
+	var RegExpFormula = new RegExp('([(][!]([A-Z]|[a-z]|[0-1])[)])|([(]([A-Z]|[a-z]|[0-1])((&)|(\\|)|(->)|(~))([A-Z]|[a-z]|[0-1])[)])', 'g');
+	var atomarExp = new RegExp('([A-Z]|[a-z]|[0-1])', 'g');
+	var replaceFormula = "F";
+	var result;
+	var tempFormula;
+	var formulaClone = formula;
+
+	while (formulaClone != tempFormula ) {
+			tempFormula = formulaClone;
+			formulaClone = formulaClone.replace(RegExpFormula, replaceFormula);
+			}
+	  
+    tempFormula=0;  
+    var resultType = formulaClone.match(new RegExp(atomarExp, 'g'));
+    if ((formulaClone.length == 1) && (resultType != null) && (resultType.length == 1)) {
+        return true;
+    } 
+	return false;
 }
